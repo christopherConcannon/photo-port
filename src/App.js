14c99f51c2599c5140e76b40aa0008f34f55f8fa
xtophe xanon <xocon10@gmail.com>
2020-09-26 17:10:45 -0500

    make ContactForm a controlled component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav'
 import Gallery from './components/Gallery'
 import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   // you don't have to assign a method name to set the state, you can just use useState to store the initial state.  here the array of objects is assigned to the variable categories which is the only member of the array returned by useState (which usually has 2 elements)
@@ -35,8 +36,9 @@ function App() {
         currentCategory={currentCategory}
       />
       <main>
+        <Contact />
         <Gallery currentCategory={currentCategory}/>
-        <About/>
+        <About />
       </main>
     </div>
   );
